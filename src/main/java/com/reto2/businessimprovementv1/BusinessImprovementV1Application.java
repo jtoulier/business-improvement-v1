ef29cda3e89317065013fa70ef0a18a442cd87e6
Joseph Toulier Pighi <jtoulier@gmail.com>
2021-02-07 18:55:08 -0500

    Empezando

---
+++ src/main/java/com/reto2/businessimprovementv1/BusinessImprovementV1Application.java
@@ -0,0 +1,13 @@
+package com.reto2.businessimprovementv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessImprovementV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessImprovementV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto2/businessimprovementv1/BusinessImprovementV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto2.businessimprovementv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessImprovementV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
